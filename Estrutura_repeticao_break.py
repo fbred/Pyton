91fa0e6b4513649cc4fa02141171de00a25b758a
fbred <fabio.ln.nascimento@gmail.com>
2025-09-01 07:12:33 -0300

    For, While, break e continue

---
+++ Estrutura_repeticao_break.py
@@ -0,0 +1,13 @@
+while True:
+    numero = int(input("Informe um númeo:  "))
+
+    if numero == 10:
+        break
+    print(numero)
+
+
+for numero in range(10):
+
+    if numero == 5:
+        continue
+    print(numero)
