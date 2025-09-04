a09319495448734fc222f974686e2ec7e049b4b7
fbred <fabio.ln.nascimento@gmail.com>
2025-09-04 06:01:56 -0300

    Listas, matriz e fatiamento de lista

--- 01-Estrutura de Dados/00_declarando_listas.py
+++ 01-Estrutura de Dados/00_declarando_listas.py
@@ -1,4 +1,4 @@
-frutas = ["laranja", "banana", "uva"]
+frutas = ["laranja", "banana", "uva","carambola","Melão"]
 print(frutas)
 
 frutas2=[]
@@ -14,3 +14,25 @@ carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
 print(carro)
 
 print(frutas[2])
+print(frutas[-1])
+
+#lista/matriz 
+matriz = [
+    [1, "a", 2],
+    ["b", 3, 4],
+    [6, 5, "C"]
+]
+
+print(matriz[0])
+print(matriz[0][0])
+print(matriz[0][-1])
+print(matriz[-1][-1])
+
+#fatiamento de lista
+lista = ["p", "y", "t", "h", "o", "n"]
+print(lista[2:])
+print(lista[:2])
+print(lista[1:3])
+print(lista[0:3:2])
+print(lista[::])
+print(lista[::-1])
--- 01-Estrutura de Dados/Select.sql
+++
@@ -1,3 +0,0 @@
-USE SATKPARAISO
-select Cod_produto,* from tbprodutoref
-where Layout_etiqueta_int = '4006'
