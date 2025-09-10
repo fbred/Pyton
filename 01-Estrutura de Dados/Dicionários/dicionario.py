0ba1752ae5e23c721407b904825c018daf0dd9f2
fbred <fabio.ln.nascimento@gmail.com>
2025-09-10 06:02:08 -0300

    Dicionários

---
+++ 01-Estrutura de Dados/Dicionários/dicionario.py
@@ -0,0 +1,35 @@
+#formas de declarar um dicionário
+pessoa = {"nome": "João", "idade": 30, "cidade": "São Paulo"}
+print(pessoa)
+
+pessoa2 = dict(nome="Maria", idade=25, cidade="Rio de Janeiro")
+print(pessoa2)
+
+pessoa3 = dict()#dicionário vazio
+pessoa3["idade"] = 28
+pessoa3["nome"] = "Ana"
+print(pessoa3)
+
+#acessar valores
+pessoa4 = {"nome": "Carlos", "idade": 40, "cidade": "Belo Horizonte"}
+print(pessoa4["nome"]) #Acessando pelo nome da chave
+print(pessoa4["idade"]) #Acessando pela idade
+print(pessoa4["cidade"]) #Acessando pela cidade
+
+pessoa5 = dict(nome="Luiza", idade=22, cidade="Curitiba")
+print(pessoa5.get("nome")) #Acessando pelo nome da chave
+print(pessoa5.get("idade")) #Acessando pela idade
+print(pessoa5.get("cidade")) #Acessando pela cidade
+
+# Exemplo de uso de dicionário
+contatos = {
+    "guilherme": {"email": "guilherme@example.com", "telefone": "1234-5678", "endereco": "Rua A, 123"},#dicionario dentro de dicionário
+    "ana": {"email": "ana@example.com", "telefone": "9876-5432", "endereco": "Rua B, 456"},
+    "maria": {"email": "maria@example.com", "telefone": "5555-5555", "endereco": "Rua C, 789"},
+    "pedro": {"email": "pedro@example.com", "telefone": "1111-1111", "endereco": "Rua D, 101"}
+}
+#Acessando um dicionário dentro de outro(aninhado)
+print(contatos["guilherme"]["telefone"]) # Acessando os dados de Guilherme
+print(contatos["ana"]["email"]) # Acessando os dados de Ana
+print(contatos["maria"]["endereco"]) # Acessando os dados de Maria
+print(contatos["pedro"]["telefone"]) # Acessando os dados de Pedro
