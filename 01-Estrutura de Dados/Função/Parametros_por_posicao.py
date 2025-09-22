801e2c80daccbf2d2f1a88b6fd01b9960cf546ba
fbred <fabio.ln.nascimento@gmail.com>
2025-09-22 07:03:47 -0300

    Parâmetros por posição

---
+++ 01-Estrutura de Dados/Função/Parametros_por_posicao.py
@@ -0,0 +1,6 @@
+def criar_carro(modelo, ano, placa, /,marca, motor, combustivel):
+    print(modelo, ano, placa, marca, motor, combustivel)
+
+
+criar_carro("Pálio", 1999, "ABC-1234", marca="fiat", motor="1.0", combustivel="Gasolina")
+# criar_carro(Modelo="Pálio", ano=1999, placa="ABC-1234", marca="fiat", motor="1.0", combustivel="Gasolina")#da erro pois os primeiros parâmetos não aceita com a chave
