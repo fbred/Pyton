dbd68f7a938b7a17c25262c38aae53d358917426
fbred <fabio.ln.nascimento@gmail.com>
2025-09-23 06:09:15 -0300

    Utilizando a função de várias formas

--- 01-Estrutura de Dados/Função/objetos_de_primeira_classe.py
+++ 01-Estrutura de Dados/Função/objetos_de_primeira_classe.py
@@ -1,8 +1,17 @@
 def somar (a, b):
     return a + b
 
+def subtrair (a,b):
+    return a -b
+
 def exibir_resultado(a, b, funcao):
     resultado = funcao(a,b)
-    print(f"O resultado da operação {a} + {b} = {resultado}")
+    print(f"O resultado da operação é = {resultado}")
 
-    exibir_resultado(10, 10, somar)
+
+exibir_resultado(10, 10, somar)
+exibir_resultado(10, 5, subtrair)
+
+op = somar # é possível atribuir uma função em uma variável
+
+print(op(5,25)) #neste caso a variável "op" recebe a função somar
