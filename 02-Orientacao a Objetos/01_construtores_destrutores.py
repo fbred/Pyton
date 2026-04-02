b7bd48bd1cda0d8bf006bd82201a18ca5cbb74de
fbred <fabio.ln.nascimento@gmail.com>
2026-04-02 07:04:57 -0300

    Construtor e destrutor

--- 02-Orientacao a Objetos/01_construtores_destrutores.py
+++ 02-Orientacao a Objetos/01_construtores_destrutores.py
@@ -1,17 +1,14 @@
-class cachorro:
-    def __init__(self, nome, cor, acordado = True): #sempre o construtor inicializador vai ser executado primeiro na instancia da classe
-        print("Inicializando a classe")
-        self.nome=nome
-        self.cor=cor
-        self.acordado=acordado
+class Cachorro():
+    def __init__(self, nome, cor, acordado=True):
+        self.nome = nome
+        self.cor = cor
+        self.acordado = acordado
+        
+    def __del__(self):
+        print("Destruindo a instancia da classe")
 
-    def lartir(self):
+    def falar(self):
         print("latindo")
 
-    def __del__(self):
-        print("Removendo a instancia da classe")
-
-c1 = cachorro("chapie","Caramelo")     
-
-c1.lartir()
-
+c = Cachorro("Chapie","caramelo")
+c.falar()
