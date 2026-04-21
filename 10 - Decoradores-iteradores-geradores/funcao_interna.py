9661492fd8cffb854a912abfb2ef54cf0136dbe1
fbred <fabio.ln.nascimento@gmail.com>
2026-04-21 07:02:15 -0300

    Função dentro de função, ou função interna

---
+++ 10 - Decoradores-iteradores-geradores/funcao_interna.py
@@ -0,0 +1,14 @@
+def principal():
+    print('executando a funcão principal')
+
+    def funcao_interna():
+        print('executando a funcão interna')
+
+    def funcao_2():
+        print('executanto da função 2')
+        
+        
+    funcao_interna()
+    funcao_2()
+
+principal()
