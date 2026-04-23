fb58e987e9ae3c67ce21a9bccc1b60e6f10b7a0e
fbred <fabio.ln.nascimento@gmail.com>
2026-04-23 06:17:09 -0300

    Iteradores e funcionalidades

---
+++ 10 - Decoradores-iteradores-geradores/iteradores.py
@@ -0,0 +1,18 @@
+class MeuIterador:
+    def __init__(self, numeros: list[int]): #inicializador da classe
+        self.numeros = numeros
+        self.contador = 0
+
+    def __iter__(self): #iterador (é obrigatório ter)
+        return self
+    
+    def __next__(self): #iterador (é obrigatorio ter se for fazer um iterável)
+        try:
+            numeros = self.numeros[self.contador]
+            self.contador += 1
+            return numeros * 2
+        except IndexError:
+            raise StopIteration
+
+for i in MeuIterador(numeros=[38, 13, 11]):
+    print(i)
